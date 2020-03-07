16d1de3b1472fbd93c28fc2c7894eebc25466f00
newckdgns <newckdgns@naver.com>
2020-03-08 06:55:12 +0900

    Updates

---
+++ precache-manifest.93cd7c8e275a5d84bec852a8cb0ee02f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2760c8e5a9b1eaa18aa50f9f5fad95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74ab8807d5f9d0b9e647",
+    "url": "/static/css/2.568f4329.chunk.css"
+  },
+  {
+    "revision": "74ab8807d5f9d0b9e647",
+    "url": "/static/js/2.bdb4287f.chunk.js"
+  },
+  {
+    "revision": "c88806e1d35a417f49d26570f601de69",
+    "url": "/static/js/2.bdb4287f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14628c9ad299636270db",
+    "url": "/static/js/main.98bc5afd.chunk.js"
+  },
+  {
+    "revision": "31fc39c20dcc6adb44b7",
+    "url": "/static/js/runtime-main.86494460.js"
+  }
+]);
